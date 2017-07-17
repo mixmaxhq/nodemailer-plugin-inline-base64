c1678b8c8e82c0a95e3892fb9c6ed9fa8908d9a0
Matteo Brusamolin <info@alloy.it>
2017-07-17 11:29:11 +0200

    Use single or double quotes

--- src/replaceBase64Images.js
+++ src/replaceBase64Images.js
@@ -1,5 +1,5 @@
  var replaceBase64Images = function(html, getCid) {
-   return html.replace(/(<img[\s\S]*? src=")data:(image\/(?:png|jpe?g|gif));base64,([\s\S]*?)("[\s\S]*?>)/g, function(g, start, mimeType, base64, end) {
+   return html.replace(/(<img[\s\S]*? src=['"])data:(image\/(?:png|jpe?g|gif));base64,([\s\S]*?)(['"][\s\S]*?>)/g, function(g, start, mimeType, base64, end) {
      return start + 'cid:' + getCid(mimeType, base64) + end;
    });
  };
