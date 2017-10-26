0342a9ecfcc6a8af3485365f90400f21527d0ff1
Jule Marcoueille <marcou_j@btc.net>
2017-10-26 15:22:42 +0200

    Add css base64 image support and all quoting style support.

--- src/replaceBase64Images.js
+++ src/replaceBase64Images.js
@@ -1,7 +1,13 @@
- var replaceBase64Images = function(html, getCid) {
-   return html.replace(/(<img[\s\S]*? src=")data:(image\/(?:png|jpe?g|gif));base64,([\s\S]*?)("[\s\S]*?>)/g, function(g, start, mimeType, base64, end) {
-     return start + 'cid:' + getCid(mimeType, base64) + end;
-   });
- };
+var replaceBase64Images = function(html, getCid) {
+  //Replace html base64 images
+  var result = html.replace(/(<img[\s\S]*? src=['"])data:(image\/(?:png|jpe?g|gif));base64,([\s\S]*?)(['"][\s\S]*?>)/g, function(g, start, mimeType, base64, end) {
+    return start + 'cid:' + getCid(mimeType, base64) + end;
+  });
 
- module.exports = replaceBase64Images;
+  //Replace css base64 images
+  return result.replace(/(url\(['"]?)data:(image\/(?:png|jpe?g|gif));base64,([\s\S]*?)(['"]?\))/g, function(g, start, mimeType, base64, end) {
+    return start + 'cid:' + getCid(mimeType, base64) + end;
+  });
+};
+
+module.exports = replaceBase64Images;
